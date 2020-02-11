ce430685218f9cb255dfa5197b149d337a11c3e6
Justin Carlson <jcarlson@secureworks.com>
2020-02-11 12:23:41 -0500

    All of it working in ng8

--- src/netrunner/netrunner_module.ts
+++ src/netrunner/netrunner_module.ts
@@ -8,8 +8,9 @@ import {SearchCmp} from './search_cmp';
 import {CardCmp} from './card_cmp';
 import {NetrunnerService} from './netrunner_service';
 
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatCardModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatPaginatorModule} from '@angular/material/paginator';
